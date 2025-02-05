052d361a0686a75ee4e9aa0b8829a469f0c185ba
Landon Davidson <accounts@landond.tech>
2025-02-04 21:42:03 -0800

    Add: rough draft resize

--- ChainingHashTable.java
+++ ChainingHashTable.java
@@ -26,7 +26,38 @@ public class ChainingHashTable <K,V> implements DeletelessDictionary<K,V>{
 
     // Function to resize the hash table
     private void resize() {
-        // TODO
+        // Copy current keys and values
+        List<K> keys = getKeys();
+        List<V> values = getValues();
+
+        // Find the next prime from the list, if there isn't use a power of 2 + 1
+        int primei = 0;
+        for (; primei < primes.length; primei++) {
+            // If the current prime is the length of the table then increment primei to get the next one and break
+            if (table.length == primes[primei]) {
+                primei++;
+                break;
+            }
+        }
+        // If the next prime is in the predefined array create a new table of that size
+        if (primei < primes.length) {
+            table = (LinkedList<Item<K,V>>[]) Array.newInstance(LinkedList.class, primes[primei]);
+        // Otherwise calculate the next power of 2 + 1 and create a new table of that size
+        } else {
+            // Find the next power of two - 1 by oneing out every bit after the most significant bit. 
+            // Then add 2 to get the next power of 2 + 1.
+            int next_size = table.length;
+            for (int shift = 1; shift < 32; shift *= 2) {
+                next_size |= next_size >> shift;
+            }
+            next_size += 2;
+            table = (LinkedList<Item<K,V>>[]) Array.newInstance(LinkedList.class, next_size);
+        }
+
+        // Now iterate through all of the old keys and values and insert them into the new table
+        for (int i = 0; i < size; i++) {
+            insert(keys.get(i), values.get(i));
+        }
     }
 
     public V insert(K key, V value){
@@ -48,6 +79,7 @@ public class ChainingHashTable <K,V> implements DeletelessDictionary<K,V>{
         // If we made it through the loop then the key doesn't exist yet and we can add it to the head of the bucket
         Item<K, V> new_item = new Item<K, V>(key, value);
         bucket.add(new_item);
+        size++;
 
         // There was no old value so return null
         return null;
